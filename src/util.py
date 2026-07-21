43f151eb4dedd5f4dbecb6ee69fbe1c5de227614
Lexcellent <2587936056@qq.com>
2026-07-21 13:37:21 +0800

    服务器节点获取重试次数改为3次

--- src/util.py
+++ src/util.py
@@ -227,7 +227,7 @@ def test_cdn_server(server: ContentServer, depot_id: str, chunk_sha: str):
         start = time.perf_counter()
         resp = get_httpx_client().get(url, timeout=30)
         cost = time.perf_counter() - start
-        if resp.status_code == 200:
+        if resp.status_code == 200 and cost < 5:
             return {
                 "host": server.host,
                 "time": cost,
@@ -239,7 +239,7 @@ def test_cdn_server(server: ContentServer, depot_id: str, chunk_sha: str):
 
 
 def get_content_servers(cell_id: int, num_servers=40):
-    for _ in range(5):
+    for _ in range(3):
         try:
             return get_content_servers_from_webapi(cell_id, num_servers=num_servers)
         except requests.exceptions.ConnectTimeout as _:
@@ -257,14 +257,13 @@ def refresh_cdn_server():
         result = test_cdn_server(server, "3167021", "ccb1bf52956792fac3372220ced49880e0bcec2b")
         if result:
             results.append(result)
+    servers = get_content_servers(33)
+    for server in servers:
+        result = test_cdn_server(server, "3167021", "ccb1bf52956792fac3372220ced49880e0bcec2b")
+        if result:
+            results.append(result)
     if len(results) == 0:
-        servers = get_content_servers(33)
-        for server in servers:
-            result = test_cdn_server(server, "3167021", "ccb1bf52956792fac3372220ced49880e0bcec2b")
-            if result:
-                results.append(result)
-        if len(results) == 0:
-            raise Exception("未找到合适的下载节点,请检查网络情况是否能访问https://api.steampowered.com/IContentServerDirectoryService/GetServersForSteamPipe/v1/")
+        raise Exception("未找到合适的下载节点,请检查网络情况是否能访问https://api.steampowered.com/IContentServerDirectoryService/GetServersForSteamPipe/v1/")
     results.sort(key=lambda x: x["time"])
     logger.debug(results)
     global STEAM_DOWNLOAD_SERVERS
